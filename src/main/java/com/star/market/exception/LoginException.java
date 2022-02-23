44eb6d52e8af6d7ead72e3c62a502b7bc9ee8c0c
dawn <dawn@atguigu.com>
2022-02-23 13:14:13 +0800

    hot-fix commit1

--- src/main/java/com/star/market/exception/LoginException.java
+++ src/main/java/com/star/market/exception/LoginException.java
@@ -3,6 +3,9 @@ package com.star.market.exception;
 public class LoginException extends Exception{
 
     public LoginException() {
+        System.out.println("你好");
+        System.out.println("你好1");
+        System.out.println("你好2");
     }
 
     public LoginException(String message) {
